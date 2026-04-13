fb9b0bae670c9f9950f471fb79c02ecfbc1545ed
Isaac Toro <torodevelopmentbcn@gmail.com>
2026-04-13 20:50:42 +0000

    domain generator finished _ Isaac

--- src/app.js
+++ src/app.js
@@ -10,13 +10,15 @@ window.onload = function() {
   console.log("Hello Rigo from the console!");
   const pronoun = ['the', 'our', 'my', 'her', 'your', 'his', 'their', 'its', 'that', 'this'];
   const adj = ['small', 'hot', 'huge', 'tiny', 'spicy', 'soggy', 'cursed', 'thicc', 'crusty', 'suspicious', 'forbidden', 'blessed', 'haunted', 'sweaty', 'drunk', 'naked', 'gassy', 'chunky', 'greasy', 'chaotic'];
-  const noun = ['cat', 'burrito', 'grandma', 'raccoon', 'toilet', 'goblin', 'avocado', 'hamster', 'mullet', 'toenail', 'armpit', 'hedgehog', 'waffle', 'llama', 'pickle', 'clown', 'sloth', 'nugget', 'potato', 'cactus', 'booze', 'pigeon', 'walrus', 'noodle', 'pancake'];
+  const noun = ['deadlift', 'squat', 'benchpress', 'barbell', 'plates', 'chalk', 'belt', 'singlet', 'gains', 'pr', 'maxout', 'grindstone', 'repdeath', 'legday', 'spotter', 'rack', 'collar', 'lockout', 'bracedbelt', 'kneewrap', 'sumo', 'conventional', 'rawlifter', 'equipped', 'totalweight'] ;
+  const ext = ['.com', '.net', '.io', '.gg', '.wtf', '.lol', '.xyz', '.fit', '.gym', '.club', '.pr', '.rm', '.rpe'];
 
   let domains = [];
   for (let p = 0; p < pronoun.length; p++) {
     for (let a = 0; a < adj.length; a++) {
       for (let n = 0; n < noun.length; n++) {
-        domains.push(pronoun[p] + adj[a] + noun[n] + '.com');
+         let randomExt = ext[Math.floor(Math.random() * ext.length)];
+        domains.push(pronoun[p] + adj[a] + noun[n] + randomExt);
       }
     }
   }
